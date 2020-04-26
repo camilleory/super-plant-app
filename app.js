ea2bb258a490b6a1d80d7ae7c624b558ccc2ca31
Manuel <mbuergel@gmx.de>
2020-04-26 12:48:06 +0200

    added login, logout, basic authentication with local strategy

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const bodyParser    = require('body-parser');
+const cookieParser  = require('cookie-parser');
+const express       = require('express');
+const favicon       = require('serve-favicon');
+const hbs           = require('hbs');
+const mongoose      = require('mongoose');
+const logger        = require('morgan');
+const path          = require('path');
+const passport      = require("passport");
+const session       = require('express-session');
+
+const LocalStrategy = require('passport-local').Strategy;
+const MongoStore    = require("connect-mongo")(session);
+const User          = require("./models/userModel");
+const bcrypt        = require('bcrypt');
+const flash         = require('connect-flash');
 
 
 mongoose
@@ -30,29 +38,87 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// Express View engine setup
+app.use(flash());
 
+// Express View engine setup
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
       
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
+// express-session configuration --> use mongostore in the setup then sessions get stored
+app.use(
+  session({
+    secret: "supersecret",
+    cookie: { maxAge: 24 * 60 * 60 * 1000 }, // 1 day
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      resave: true,
+      saveUninitialized: false,
+      ttl: 24 * 60 * 60, // 1 day
+    }),
+  })
+);
+
+// associate user with a session // store the user into the session
+passport.serializeUser((user, callback) => {
+  callback(null, user._id);
+});
+
+passport.deserializeUser((id, callback) => {
+  User.findById(id)
+    .then((user) => {
+      callback(null, user);
+    })
+    .catch((error) => {
+      callback(error);
+    });
+});
+
+
+// local strategy ===> how to move the strategies to a different folder an require it here??? ask Hendrik / Mir
+
+passport.use(
+  new LocalStrategy((username, password, callback) => {
+    User.findOne({ username })
+      .then((user) => {
+        if (!user) {
+          console.log("naaaah")
+          return callback(null, false, { message: "No such user" });
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+          console.log("yeeesh")
+          return callback(null, false, { message: "Wrong password" });
+        }
+        callback(null, user);
+      })
+      .catch((error) => {
+        callback(error);
+      });
+  })
+);
+
+
+//Passport setup
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
+app.locals.title = "PlantApp - Plants Love It";
 
 
 const index = require('./routes/index');
 app.use('/', index);
 
+const authRoutes = require('./routes/auth.routes');
+app.use('/', authRoutes);
 
 module.exports = app;
---
+++ routes/auth.routes.js
@@ -0,0 +1,67 @@
+// routes/auth-routes.js
+const express = require("express");
+const router = express.Router();
+
+// User model
+const User = require("../models/userModel");
+
+// Bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+//passport setup
+const passport      = require("passport");
+const session       = require('express-session');
+const flash         = require('connect-flash');
+
+
+//signup and stay logged in
+router.get("/signup", (req, res) => {
+  res.render("auth/signup");
+});
+
+router.post("/signup", (req, res) => {
+  const salt = bcrypt.genSaltSync(bcryptSalt);
+  const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+  let user = new User({
+    username: req.body.username,
+    password: hashedPassword,
+  });
+
+  user.save().then((signedUpUser) => {
+    req.login(signedUpUser, () => {
+      res.redirect('/');
+    });
+  });
+});
+
+
+//login
+router.get("/login", (req, res, next) => {
+  res.render("auth/login", { message: req.flash('error')} );
+  // console.log(req.flash('error'))
+  // flash errors are always an array!!!
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+    passReqToCallback: true,
+  })
+);
+
+// logout
+router.get("/logout", (req, res) => {
+  req.logout()
+  res.redirect("/")
+})
+
+
+
+module.exports = router;
+
+
