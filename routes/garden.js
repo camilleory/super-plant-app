c13682eb5bb5adf13a9da0fa0b2fe38b6b4c9957
Manuel <mbuergel@gmx.de>
2020-04-29 09:52:27 +0200

    changed standard routes for garden / plants

--- routes/garden.js
+++ routes/garden.js
@@ -1,57 +1,56 @@
-const express       = require("express");
-const router        = express.Router();
-const axios         = require('axios')
-const Plant         = require('../models/plantModel')
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+const Plant = require("../models/plantModel");
 
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  } else {
-    res.redirect('/login')
-  }
-}
+// every route below ist protected through this middleware
+// router.use((req, res, next) => {
+//   if (req.isAuthenticated()) {
+//     next();
+//   } else {
+//     res.redirect("auth/login");
+//   }
+// });
 
-// router.get("/", (req, res) => {
-//   res.render("garden/garden", {user: req.user}) 
-// })
+router.get("/garden", (req, res, next) => {
+  res.render("garden/garden");
+});
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   Plant.find().then((plants) => {
+    console.log("plants from Database:", plants + plants.length);
 
-    console.log("plants from Database:", plants + plants.length)
-
-    res.render('garden/garden', {
-      plantsList: plants
+    res.render("garden/garden", {
+      plantsList: plants,
     });
   });
 });
 
 // AddPlant GET REQUEST (CREATE)
 
-router.get('/addPlant', (req, res, next) => {
-
-  res.render('garden/addPlant');
+router.get("/addPlant", (req, res) => {
+  res.render("garden/addPlant");
 });
-
 
 // AddPlant POST Request : User can enter the common or scientific name of the plant and we give him a list of all plants with this name
 
-router.post('/addPlant', (req, res) => {
-
-  axios.get('https://trefle.io/api/plants/', {
-    params: {
-      q: req.body.common_name,
-      token: "ckZrTGRTdWdKKzVUenNvOVVqOFRGdz09"
-    }
-  }).then((response) => {
-    console.log("This is the response from API", response.data)
-    res.render('garden/chosePlant', {
-      response: response.data
+router.post("/addPlant", (req, res, next) => {
+  axios
+    .get("https://trefle.io/api/plants/", {
+      params: {
+        q: req.body.common_name,
+        token: "ckZrTGRTdWdKKzVUenNvOVVqOFRGdz09",
+      },
     })
-  }).then (()=> {
-      console.log('ID parameter will be here')
-
-  })
+    .then((response) => {
+      console.log("This is the response from API", response.data);
+      res.render("garden/chosePlant", {
+        response: response.data,
+      });
+    })
+    .then(() => {
+      console.log("ID parameter will be here");
+    });
 });
 
 /*POST addPlant*/
@@ -72,37 +71,22 @@ router.post('/addPlant', (req, res) => {
 
 //chosePlant GET REQUEST
 
-router.get('/chosePlant', (req, res, next) => {
-
-  res.render('garden/chosePlant');
+router.get("/chosePlant", (req, res, next) => {
+  res.render("garden/chosePlant");
 });
 
 //chosePlant POST REQUEST --> Get plant ID and make a new post request to api, then save data into database
 
-
-router.post('/chosePlant', (req, res) => {
-
-  axios.get('https://trefle.io/api/plants/${req.params.selectedPlant}', (req, res) => {
-  }).then((response) => {
-    console.log("This is the response from API", response.data)
-    res.render('garden/garden', {response: response.data} )
-  });
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+router.post("/chosePlant", (req, res, next) => {
+  axios
+    .get(
+      "https://trefle.io/api/plants/${req.params.selectedPlant}",
+      (req, res) => {}
+    )
+    .then((response) => {
+      console.log("This is the response from API", response.data);
+      res.render("garden/garden", { response: response.data });
+    });
+});
 
 module.exports = router;
